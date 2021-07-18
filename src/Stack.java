f8e62c907706330e0a126660dc406a11135aefcb
rachel-lly <3556753750@qq.com>
2021-07-18 17:41:29 +0800

    Stack

---
+++ src/Stack.java
@@ -0,0 +1,39 @@
+/**
+ * @Describtion: 链表实现 Stack
+ * @Author: rachel-lly
+ * @Date: 2021-07-17 17:19
+ */
+public class Stack<T> {
+
+    private static class Node<T>{
+        T item;
+        Node<T> next;
+
+        Node(){
+            item = null;
+            next = null;
+        }
+
+        public Node(T item, Node<T> next) {
+            this.item = item;
+            this.next = next;
+        }
+
+        boolean isEnd(){
+            return item == null && next == null;
+        }
+    }
+
+    private Node<T> top = new Node<T>();
+
+    public void push(T item) {
+        top = new Node<T>(item, top);
+    }
+
+    public T pop() {
+        T result = top.item;
+        if (!top.isEnd()) top = top.next;
+        return result;
+    }
+
+}
